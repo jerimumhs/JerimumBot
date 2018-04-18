1238fe7e0226a586196a47d79a099609e4c42c95
Victor Matheus Castro <victormatheuscastro@gmail.com>
2018-04-18 19:56:07 -0300

    Add rules command

--- bot/bot.py
+++ bot/bot.py
@@ -24,6 +24,24 @@ def echo(bot, update):
     update.message.reply_text(update.message.text)
 
 
+def rules(bot, update):
+    """Send a message with the group rules."""
+    rules = (
+        "Amet fufe fit eget nifi auctor nifl nifh ifum luctuf in "
+        "fufto malefuada in tinfidunt lafinia auctor fiferra turfif "
+        "aliquam eu iaculif malefuada iaculif ifum amet fufto diam "
+        "urna fit fehicula turfif auctor eget lectuf luctuf maefenaf "
+        "a confectetur lafinia magna magna hafitafe turfif luctuf amet"
+        " luctuf efifitur malefuada lorem urna ifum fofuere in eu turfif "
+        "efifitur lorem ifum feftifulum fit aliquam fiferra maefenaf turfif "
+        "leo mi fit confectetur eu eft luctuf malefuada a faufifuf "
+        "tinfidunt nifh et urna aliquam in diam fafien dictumft fufto eget"
+        " nifh magna confectetur magna in follifitudin et fofuere euifmod "
+        "diam euifmod eft diam confectetur."
+        )
+    update.message.reply_text(rules)
+
+
 def error(bot, update, error):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, error)
@@ -37,6 +55,7 @@ def run_bot():
 
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("rules", rules))
 
     dp.add_handler(MessageHandler(Filters.text, echo))
 
