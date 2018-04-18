01614d2db35069894f4f7af8ef754d05ae3cdeb0
Victor Matheus Castro <victormatheuscastro@gmail.com>
2018-04-18 19:38:40 -0300

    Add a sample from api documentation

---
+++ bot/bot.py
@@ -0,0 +1,48 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+
+from . import config
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+def start(bot, update):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+
+def help(bot, update):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(bot, update):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def run_bot():
+    """Start the bot."""
+    updater = Updater(config['telegram']['token'])
+
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+
+    updater.idle()
+
---
+++ run_bot.py
@@ -0,0 +1,5 @@
+from bot.bot import run_bot
+
+
+if __name__ == '__main__':
+    run_bot()
