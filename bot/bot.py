f1ff77c246a0e664e4decad6b99df5561d1691bf
Victor Matheus Castro <victormatheuscastro@gmail.com>
2018-04-24 21:57:20 -0300

    Change the summary of rules

--- bot/bot.py
+++ bot/bot.py
@@ -73,12 +73,12 @@ def button(bot, update):
         bot.answer_callback_query(
             callback_query_id=query.id,
             text=(
-                "REGRAS:\n"
-                "1-Respeite os membros do grupo.\n"
-                "2-Não compartilhar conteúdo sem autorização.\n"
-                "3-Não envie SPAM.\n"
-                "4-Proibido envio de material pornográfico.\n"
-                "5-Havendo qualquer restrição as regras será banido."
+                "REGRAS:\n\n"
+                "1.Respeitar os membros do grupo\n"
+                "2.Não compartilhar conteúdo sem autorização\n"
+                "3.Não enviar Spam\n"
+                "4.Proibido envio de material pornográfico\n"
+                "5.Havendo qualquer restrição às regras, será banido"
             ),
             show_alert=True
         )
