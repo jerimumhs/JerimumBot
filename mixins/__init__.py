c46ba71abb10395a44d5c17c0ea731842263ec3c
rodrigondec <rodrigondec@gmail.com>
2019-10-04 01:01:23 -0300

    import mixins to module

---
+++ mixins/__init__.py
@@ -0,0 +1,5 @@
+from mixins.base_commands import BaseCommandsBotMixin
+from mixins.callback import CallbackBotMixin
+from mixins.error import ErrorBotMixin
+from mixins.message import MessageBotMixin
+from mixins.sticker import StickerBotMixin
