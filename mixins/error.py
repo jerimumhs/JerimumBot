09535e1b57ed250a87d5622e344459986aa5ee9c
rodrigondec <rodrigondec@gmail.com>
2019-10-04 01:01:04 -0300

    Error Handler mixin

---
+++ mixins/error.py
@@ -0,0 +1,26 @@
+import logging
+
+from core import BotTelegramCore
+from messages import ERROR_BLOCKED, ERROR_INITIATE
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+class ErrorBotMixin(BotTelegramCore):
+    def config_handlers(self):
+        logging.info('Configurando error handler do bot...')
+        self.updater.dispatcher.add_error_handler(self.__class__.error)
+
+    @staticmethod
+    def error(bot, update, err):
+        """Log Errors caused by Updates."""
+        if err.message == "Forbidden: bot can't initiate conversation with a user":
+            update.message.reply_text(ERROR_INITIATE)
+        elif err.message == "Forbidden: bot was blocked by the user":
+            update.message.reply_text(ERROR_BLOCKED)
+        else:
+            logger.warning('Update "%s" caused error "%s"', update, err)
