110d9ce36278bb93599deba920ce87f70a6b77ec
rodrigondec <rodrigondec@gmail.com>
2019-10-04 23:35:32 -0300

    import each handler on list

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,6 +1,14 @@
 from commands.base import config_handlers as base_handler
+from commands.callback import config_handlers as callback_handler
+from commands.error import config_handlers as error_handler
+from commands.message import config_handlers as message_handler
+from commands.sticker import config_handlers as sticker_handler
 
 
 handlers = [
-    base_handler
+    base_handler,
+    callback_handler,
+    error_handler,
+    message_handler,
+    sticker_handler
 ]
