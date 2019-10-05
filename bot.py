82c0b1157b6102eeea66a325fe3bea21a37224df
rodrigondec <rodrigondec@gmail.com>
2019-10-04 23:35:47 -0300

    Bot config_handlers with commands modules

--- bot.py
+++ bot.py
@@ -1,16 +1,10 @@
 from core import BotTelegramCore
-from mixins import (BaseCommandsBotMixin, CallbackBotMixin, ErrorBotMixin,
-                    MessageBotMixin, StickerBotMixin)
 from commands import handlers
 
 
-class JerimumBot(CallbackBotMixin, ErrorBotMixin,
-                 MessageBotMixin, StickerBotMixin):
+class JerimumBot(BotTelegramCore):
     """Bot Controller"""
 
     def config_handlers(self):
-        for BaseClass in self.__class__.__bases__:
-            assert issubclass(BaseClass, BotTelegramCore)
-            BaseClass.config_handlers(self)
-        for handler in handlers:
-            handler(self)
+        for config_handler in handlers:
+            config_handler(self)
