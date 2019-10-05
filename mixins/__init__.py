7770f0e4792a1c615b9ba038ad180c0ee853231d
rodrigondec <rodrigondec@gmail.com>
2019-10-04 23:35:02 -0300

    removed mixins & base_commands

--- mixins/__init__.py
+++
@@ -1,5 +0,0 @@
-from mixins.base_commands import BaseCommandsBotMixin
-from mixins.callback import CallbackBotMixin
-from mixins.error import ErrorBotMixin
-from mixins.message import MessageBotMixin
-from mixins.sticker import StickerBotMixin
