5bbc0748f29588a69266258f61e8910fe47ef6b2
fguisso <fguisso@gmail.com>
2019-10-05 17:28:40 -0300

    commands: add master coach

--- commands/__init__.py
+++ commands/__init__.py
@@ -3,12 +3,13 @@ from commands.callback import config_handlers as callback_handler
 from commands.error import config_handlers as error_handler
 from commands.message import config_handlers as message_handler
 from commands.sticker import config_handlers as sticker_handler
-
+from commands.coach import config_handlers as coach_handler
 
 handlers = [
     base_handler,
     callback_handler,
     error_handler,
     message_handler,
-    sticker_handler
+    sticker_handler,
+    coach_handler
 ]
