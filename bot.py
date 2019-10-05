d9de13a0daf4f7d6cd88beebde8d3257af6bfc4a
rodrigondec <rodrigondec@gmail.com>
2019-10-04 23:07:34 -0300

    for inside bot config_handlers

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from core import BotTelegramCore
 from mixins import (BaseCommandsBotMixin, CallbackBotMixin, ErrorBotMixin,
                     MessageBotMixin, StickerBotMixin)
-from commands import config_handlers
+from commands import handlers
 
 
 class JerimumBot(CallbackBotMixin, ErrorBotMixin,
@@ -12,4 +12,5 @@ class JerimumBot(CallbackBotMixin, ErrorBotMixin,
         for BaseClass in self.__class__.__bases__:
             assert issubclass(BaseClass, BotTelegramCore)
             BaseClass.config_handlers(self)
-        config_handlers(self)
+        for handler in handlers:
+            handler(self)
