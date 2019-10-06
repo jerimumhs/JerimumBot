5adea884661993b732a95f9d1b416862ad2e526a
fguisso <fguisso@gmail.com>
2019-10-05 23:32:22 -0300

    coach: new query parameter and timeout

--- tests/test_coach.py
+++ tests/test_coach.py
@@ -8,26 +8,20 @@ class CoachTest(TestCase):
     @mock.patch('commands.coach.requests.get')
     @mock.patch('commands.coach.BeautifulSoup')
     @mock.patch('commands.coach.re')
-    def test_randint_returning_max_value(self, mocked_re, mocked_bsp, mocked_get, mocked_randint):
+    def test_randint_returning_max_value(
+            self, mocked_re, mocked_bsp, mocked_get, mocked_randint):
         self.assertIsInstance(mocked_re, mock.MagicMock)
         self.assertIsInstance(mocked_bsp, mock.MagicMock)
         self.assertIsInstance(mocked_get, mock.MagicMock)
         self.assertIsInstance(mocked_randint, mock.MagicMock)
 
         mocked_randint.side_effect = lambda x, y: y
-        mocked_bsp.return_value = mock.MagicMock(
-            find_all=mock.MagicMock(
-                return_value=[
-                    mock.MagicMock(get_text=mock.MagicMock(return_value='quote 1')),
-                    mock.MagicMock(get_text=mock.MagicMock(return_value='quote 2'))
-                ]
-            )
-        )
         mocked_bsp.find_all = mock.MagicMock(
             return_value=[
-                mock.MagicMock(get_text=mock.MagicMock(return_value='quote 1')),
-                mock.MagicMock(get_text=mock.MagicMock(return_value='quote 2'))
+                mock.MagicMock(
+                    get_text=mock.MagicMock(return_value='quote 1')),
+                mock.MagicMock(
+                    get_text=mock.MagicMock(return_value='quote 2'))
             ]
         )
-        coach(mock.MagicMock(), mock.MagicMock())
-
+        coach(mock.MagicMock(), mock.MagicMock(), mock.MagicMock())
