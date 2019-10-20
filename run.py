e094648c52cb20e2ac63795ecfe173aa6b804365
rodrigondec <rodrigondec@gmail.com>
2019-10-20 16:10:36 -0300

    port 5000 by default

--- run.py
+++ run.py
@@ -10,4 +10,4 @@ if __name__ == '__main__':
     bot = JerimumBot(token=config('BOT_TOKEN', default='??'))
 
     bot.run()
-    app.run(host='0.0.0.0', port=config('PORT', default=8443, cast=int))
+    app.run(host='0.0.0.0', port=config('PORT', default=5000, cast=int))
