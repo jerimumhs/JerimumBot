bf44c12c97bee1fe8d4df39e88fff88ac7e247a9
rodrigondec <rodrigondec@gmail.com>
2019-10-20 15:53:56 -0300

    installed flask

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+from bot import JerimumBot
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    JerimumBot.send_message(chat_id='-281314498', text='Essa é uma mensagem de teste')
+    return 'Hello, World!'
+
