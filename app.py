38e83f8e5d303719579577237a05978b6664817a
rodrigondec <rodrigondec@gmail.com>
2019-10-21 00:25:37 -0300

    app & run reworked

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
-from decouple import config
-
 from flask import Flask
 
 from bot import JerimumBot
 
 
 app = Flask(__name__)
-bot = JerimumBot(token=config('BOT_TOKEN', default='??'))
 
 
 @app.route('/')
 def hello():
     JerimumBot.send_message(chat_id='-281314498', text='Essa é uma mensagem de teste')
     return 'Hello, World!'
-
