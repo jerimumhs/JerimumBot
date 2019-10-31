cf6d134dc917d6e88b4fdce809f2592785556696
rodrigondec <rodrigondec@gmail.com>
2019-10-31 13:42:17 -0300

    instance.add_handler on handlers

--- commands/sticker.py
+++ commands/sticker.py
@@ -15,17 +15,17 @@ logger = logging.getLogger(__name__)
 def config_handlers(instance: BotTelegramCore):
     logging.info('Configurando comandos de sticker do bot...')
 
-    instance.updater.dispatcher.add_handler(
+    instance.add_handler(
         CommandHandler("xinga", lambda bot, update: bot.send_sticker(
             sticker="CAADAQADCgEAAmOWFQq4zU4TMS08AwI",
             chat_id=update.message.chat_id)))
 
-    instance.updater.dispatcher.add_handler(
+    instance.add_handler(
         CommandHandler("aberta", lambda bot, update: bot.send_sticker(
             sticker="CAADAQADXwADHaeSHfBrZMzXjtwlFgQ",
             chat_id=update.message.chat_id)))
 
-    instance.updater.dispatcher.add_handler(
+    instance.add_handler(
         CommandHandler("fechada", lambda bot, update: bot.send_sticker(
             sticker="CAADAQADYAADHaeSHb7fujge5DRfFgQ",
             chat_id=update.message.chat_id)))
