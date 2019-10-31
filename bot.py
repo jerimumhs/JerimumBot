980a9eae613a7ba14a92e032160128f3b65d3150
rodrigondec <rodrigondec@gmail.com>
2019-10-31 16:22:13 -0300

    Better SIngleton pattern & auth on BotTelegramCore
    fixes #52

--- bot.py
+++ bot.py
@@ -16,15 +16,25 @@ logger = logging.getLogger(__name__)
 class JerimumBot(BotTelegramCore):
     """Bot Controller"""
 
+    _chat_id = config('JERIMUM_CHAT_ID', default=-1001088768449, cast=int)
+
+    def __init__(self):
+        super(JerimumBot, self).__init__()
+        self._handlers_configured = False
+        self.config_handlers()
+
     def config_handlers(self):
         for config_handler in handlers:
             config_handler(self)
+        self._handlers_configured = True
+
+    @property
+    def chat_id(self):
+        return self._chat_id
 
     @classmethod
-    def instance(cls):
-        if super().instance() is None:
-            return cls(token=config('BOT_TOKEN', default='??'))
-        return super().instance()
+    def send_message(cls, text, chat_id=None, parse_mode=None):
+        super().send_message(text, chat_id or cls._chat_id, parse_mode)
 
     @classmethod
     def run(cls):
--- core/telegram.py
+++ core/telegram.py
@@ -1,6 +1,7 @@
-from abc import ABC, abstractmethod
 import logging
+from abc import ABC, abstractmethod
 
+from decouple import config
 from telegram.ext import Updater, Handler
 
 
@@ -12,76 +13,96 @@ logger = logging.getLogger(__name__)
 
 
 class BotTelegramCore(ABC):
-    __instance = None
+    _instance = None
 
     def __new__(cls, *args, **kwargs):
-        if not cls.__instance:
-            cls.__instance = super().__new__(cls)
-        return cls.__instance
+        if not cls._instance:
+            if cls == BotTelegramCore:
+                raise TypeError('Cannot initilize instance from BotTelegramCore')
+            BotTelegramCore._instance = super(BotTelegramCore, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
 
-    def __init__(self, token, port, server_url):
+    def __init__(self):
         logging.info('Inicializando o bot...')
-        self.token = token
-        self.port = port
-        self.server_url = server_url
+        self.token = config('BOT_TOKEN')
+        self.port = config('PORT', default=8443, cast=int)
+        self.server_url = config('SERVER_URL')
 
-        self.__updater = Updater(self.token)
-        self.__running = False
-        self.config_handlers()
+        self._updater = Updater(self.token)
+        self._running = False
 
     @classmethod
-    def send_message(cls, chat_id, text, parse_mode=None):
+    def instance(cls):
+        return cls._instance or cls()
+
+    @abstractmethod
+    def config_handlers(self):
+        raise NotImplementedError('Cannot call config_handler from BotTelegramCore')
+
+    @property
+    @abstractmethod
+    def chat_id(self):
+        raise NotImplementedError('Cannot call chat_id from BotTelegramCore')
+
+    @property
+    def chat(self):
+        return self._updater.bot.get_chat(self.chat_id)
+
+    @property
+    def administrators(self):
+        return [chat_member.user.id for chat_member in self.chat.get_administrators()]
+
+    @classmethod
+    def is_admin(cls, user_id):
         instance = cls.instance()
         assert isinstance(instance, BotTelegramCore)
-        instance.__updater.bot.send_message(
+        return user_id in instance.administrators
+
+    @classmethod
+    def send_message(cls, text, chat_id, parse_mode=None):
+        instance = cls.instance()
+        assert isinstance(instance, BotTelegramCore)
+        instance._updater.bot.send_message(
             chat_id=chat_id,
             text=text,
             parse_mode=parse_mode
         )
 
-    @classmethod
-    def instance(cls):
-        return cls.__instance
-
-    @abstractmethod
-    def config_handlers(self):
-        raise NotImplementedError('Cannot call config_handler from BotCore')
-
     def add_handler(self, handler: Handler):
         if not isinstance(handler, Handler):
             raise ValueError("Handler deve ser do tipo Handler!")
-        self.__updater.dispatcher.add_handler(handler)
+        self._updater.dispatcher.add_handler(handler)
 
     def add_error_handler(self, handler):
-        self.__updater.dispatcher.add_error_handler(handler)
+        self._updater.dispatcher.add_error_handler(handler)
 
     def run_web(self):
         """Start the bot as a webhook server"""
 
-        self.__updater.start_webhook(
+        self._updater.start_webhook(
             listen="0.0.0.0",
             port=self.port,
             url_path=self.token
         )
 
-        self.__updater.bot.set_webhook(f"{self.server_url}/{self.token}")
+        self._updater.bot.set_webhook(f"{self.server_url}/{self.token}")
 
         logging.info('Bot está rodando como um webserver!')
-        self.__updater.idle()
+        self._updater.idle()
 
     def run_cmd(self):
         """Start the bot as a python script loop"""
-        self.__updater.start_polling()
+        self._updater.start_polling()
 
         logging.info('Bot está rodando como um script python!')
-        self.__updater.idle()
+        self._updater.idle()
 
     def run(self):
         """Start the bot as a python script loop"""
-        if not self.__running:
-            self.__updater.start_polling()
+        if not self._running:
+            self._updater.start_polling()
 
             logging.info('Bot está rodando como um script python!')
-            self.__running = True
+            self._running = True
         else:
             logging.info('Bot já está rodando!')
