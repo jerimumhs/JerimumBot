643221869c3c586676ed3db0f60065d2d4601c03
rodrigondec <rodrigondec@gmail.com>
2019-10-31 17:14:26 -0300

    instance method

--- tests/test_singleton.py
+++ tests/test_singleton.py
@@ -7,16 +7,16 @@ from bot import JerimumBot
 class SingletonTest(TestCase):
     @mock.patch('core.telegram.Updater')
     def test_same_jerimum(self, mocked_updater):
-        i1 = JerimumBot()
-        i2 = JerimumBot()
+        i1 = JerimumBot.instance()
+        i2 = JerimumBot.instance()
         self.assertEqual(i1, i2)
 
     @mock.patch('core.telegram.Updater')
     def test_core_initialize_error(self, mocked_updater):
-        self.assertRaises(TypeError, BotTelegramCore)
+        self.assertRaises(TypeError, BotTelegramCore.instance)
 
     @mock.patch('core.telegram.Updater')
     def test_jerimum_initialize(self, mocked_updater):
-        i1 = JerimumBot()
-        i2 = BotTelegramCore()
+        i1 = JerimumBot.instance()
+        i2 = BotTelegramCore.instance()
         self.assertEqual(i1, i2)
